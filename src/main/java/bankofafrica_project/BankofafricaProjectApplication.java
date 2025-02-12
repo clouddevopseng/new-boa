e9ced504c2db6dd70c8aaf1799be541157bc75f4
venkat <venkat@tcs.com>
2025-02-12 09:23:55 +0530

    Africa Project

---
+++ src/main/java/bankofafrica_project/BankofafricaProjectApplication.java
@@ -0,0 +1,13 @@
+package bankofafrica_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankofafricaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankofafricaProjectApplication.class, args);
+	}
+
+}
